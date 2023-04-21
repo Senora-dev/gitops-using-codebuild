e3ca5c36d452783f80aab0ff1db7aa629c0965c2
ShakedBraimok <shaked.braimok@startapp.com>
2023-04-21 13:52:14 +0300

    updated branch name and name var

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 module "gitops" {
     source = "../module"
     #version = 0.0.1
-    name = "ci"
+    name = "myproject-ci"
     description = "This is our deplopyment project."
     github_repository = "https://github.com/Senora-dev/gitops-using-codebuild.git"
-    branch = "master"
+    branch = "main"
     github_event = "PULL_REQUEST_CREATED"
     buildspec_file = ".buildspec.yaml"
 
